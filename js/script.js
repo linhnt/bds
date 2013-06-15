875b92e7192b1a4dc566a559d72df5d1f05f66a6
Hieu Pham <mr_hie@yahoo.com>
2013-06-16 02:07:41 +0700

    add rectangle draw

--- js/script.js
+++ js/script.js
@@ -2,16 +2,24 @@ var map;
 var poly;
 var markerArray = new Array();
 var markerPosArray = new Array();
+var rectangleArray = new Array();
 var countMarker = 0;
+var curTab = '#func1';
+var markerImage;
 
 $(document).ready(function(){
   initialize();
   enableSelectable();
+
   $('#myTab a').click(function(e) {
     e.preventDefault();
     $(this).tab('show');
+
+    curTab = $(this).attr('href');
+
     if (map) {
       removeMarkers();
+      removeRectangles();
       $('.list-markers').empty();
     }
   })
@@ -24,14 +32,56 @@ function initialize(){
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
   map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+  // Create our "tiny" marker icon
+  markerIcon = new google.maps.MarkerImage(
+    "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAkAAAAJCAYAAADgkQYQAAAAiklEQVR42mNgQIAoIF4NxGegdCCSHAMzEC+NUlH5v9rF5f+ZoCAwHaig8B8oPhOmKC1NU/P//7Q0DByrqgpSGAtSdOCAry9WRXt9fECK9oIUPXwYFYVV0e2ICJCi20SbFAuyG5uiECUlkKIQmOPng3y30d0d7Lt1bm4w301jQAOgcNoIDad1yOEEAFm9fSv/VqtJAAAAAElFTkSuQmCC",
+    new google.maps.Size(9, 9),
+    new google.maps.Point(0, 0),
+    new google.maps.Point(4, 5)
+  );
 }
 
 function enableSelectable(){
   google.maps.event.addListener(map, 'click', function(event) {
-    addMarker(event.latLng);
-    addToList(event.latLng, countMarker);
-    showPolygon(markerPosArray);
+    switch (curTab){
+    case '#func1':
+      var size = prompt("Enter size of overlay:", "20, 20");
+      if (size){
+        addMarker(event.latLng);
+        addToList(event.latLng, countMarker);
+        addRectangle(event.latLng, size);
+      }
+      break;
+    case '#func2':
+      addMarker(event.latLng);
+      addToList(event.latLng, countMarker);
+      showPolygon(markerPosArray);
+      break;
+    }
   });
+}
+
+function addRectangle(pos, size){
+  var size = size.split(",");
+  if (isNaN(parseInt(size[0]))) size[0] = 20;
+  if (isNaN(parseInt(size[1]))) size[1] = 20;
+  var rectangle = new google.maps.Rectangle();
+  var rectOptions = {
+      strokeColor: "#FF0000",
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: "#FF0000",
+      fillOpacity: 0.35,
+      map: map,
+      // editable: true,
+      bounds: new google.maps.LatLngBounds(
+        new google.maps.LatLng(pos.lat() - size[0]/2000, pos.lng() - size[1]/2000),
+        new google.maps.LatLng(pos.lat() + size[0]/2000, pos.lng() + size[1]/2000)
+      )
+    };
+    rectangle.setOptions(rectOptions);
+    rectangleArray.push(rectangle);
 }
 
 function showPolygon(posArray){
@@ -52,14 +102,15 @@ function showPolygon(posArray){
 
 function addToList(pos, count){
   var html = '<li class="marker-' + countMarker + '">' + countMarker + '.  ' + Math.round(pos.lat() * 1000000)/1000000 + ',' + Math.round(pos.lng() * 1000000)/1000000 + ' <a href="javascript:void(0)" onclick="return removeMarker(' + count + ')">remove</a></li>';
-  $('.list-markers').append(html);
+  $(curTab + ' .list-markers').append(html);
 }
 
 function addMarker(location){
   var marker = new google.maps.Marker({
       position: location,
       map: map,
-      title: 'marker ' + (countMarker + 1)
+      title: 'marker ' + (countMarker + 1),
+      icon: markerIcon
   });
   markerArray.push(marker);
   markerPosArray.push(new google.maps.LatLng(location.lat(), location.lng()));
@@ -76,8 +127,21 @@ function removeMarkers(){
   markerArray.length = 0;
   markerPosArray.length = 0;
   countMarker = 0;
-  showPolygon(markerPosArray);
+  if (curTab == '#func1'){
+    removeRectangles();
+  } else {
+    showPolygon(markerPosArray);
+  }
   return false;
+}
+
+function removeRectangles(){
+  for (i in rectangleArray){
+    if (rectangleArray[i] != null){
+      rectangleArray[i].setMap(null)
+    }
+  }
+  rectangleArray.length = 0;
 }
 
 function removeMarker(iMarker){
@@ -86,7 +150,13 @@ function removeMarker(iMarker){
     markerArray[iMarker - 1] = null;
     updatePosArray();
     $('.marker-' + iMarker).remove();
-    showPolygon(markerPosArray);
+
+    if (curTab == '#func1'){
+      rectangleArray[iMarker - 1].setMap(null);
+      rectangleArray[iMarker - 1] = null;
+    } else{
+      showPolygon(markerPosArray);
+    }
     return true
   }
   return false
